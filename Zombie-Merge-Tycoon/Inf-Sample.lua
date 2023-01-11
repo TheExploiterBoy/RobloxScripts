ce0d8dccd9cf02ae87654873ca8892a5288ae7f2
YellowGreg <101320329+YellowGreg@users.noreply.github.com>
2023-01-11 18:25:34 -0500

    Update Inf-Sample.lua

--- Zombie-Merge-Tycoon/Inf-Sample.lua
+++ Zombie-Merge-Tycoon/Inf-Sample.lua
@@ -2,7 +2,7 @@ task.spawn(function()
         while task.wait() do
                 local args = {[1] = 14,[2] = Vector3.new(1, 1, 1)}
                 game:GetService("ReplicatedStorage").Signals.RemoteEvents.GetWoolRemote:FireServer(unpack(args))
-            end
-        end
-    end)
+                        end
+                end
+        end)
 end)
