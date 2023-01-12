dde827df038568fc88f8c3b9978fad7c8efd416a
YellowGreg <101320329+YellowGreg@users.noreply.github.com>
2023-01-12 10:08:38 -0500

    Update FastFire.lua

--- Arsenal/FastFire.lua
+++ Arsenal/FastFire.lua
@@ -1,8 +1,5 @@
 local replicationstorage = game.ReplicatedStorage
 for i, v in pairs(replicationstorage.Weapons:GetDescendants()) do
-   if v.Name == "Auto" then
-       v.Value = true
-   end
    if v.Name == "FireRate" then
        v.Value = 0.02
    end
