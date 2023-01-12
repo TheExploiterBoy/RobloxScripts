06fd08627546bff2880be556fb88d2d4fce28fe1
YellowGreg <101320329+YellowGreg@users.noreply.github.com>
2023-01-11 19:27:59 -0500

    Delete Zombie-Merge-Tycoon directory

--- Zombie-Merge-Tycoon/Inf-Sample.lua
+++
@@ -1,8 +0,0 @@
-task.spawn(function()
-        while task.wait() do
-                local args = {[1] = 14,[2] = Vector3.new(1, 1, 1)}
-                game:GetService("ReplicatedStorage").Signals.RemoteEvents.GetWoolRemote:FireServer(unpack(args))
-                        end
-                end
-        end)
-end)
