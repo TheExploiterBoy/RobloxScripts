f70dcd0cfd3cfbdf86023271dcf8cf8dbd99beed
YellowGreg <101320329+YellowGreg@users.noreply.github.com>
2023-01-13 12:09:03 -0500

    Update inf ammo.lua

--- Arsenal/inf ammo.lua
+++ Arsenal/inf ammo.lua
@@ -1,7 +1,7 @@
 --// Credit To MMSVon For Making The Script \\--
 
 game:GetService("RunService").Stepped:connect(function() -- Loop made by MMSVon
-    pcall(function()
+    pcall(function() -- It is used for calling functions that may possibly error so that it does not stop the script and we can handle it manually.
         game:GetService("Players").LocalPlayer.PlayerGui.GUI.Client.Variables.ammocount.Value = 999
         game:GetService("Players").LocalPlayer.PlayerGui.GUI.Client.Variables.ammocount2.Value = 999
       end) 
