d325d583bed500b4cd399a9e8860ff28dbe86da6
YellowGreg <101320329+YellowGreg@users.noreply.github.com>
2023-01-17 09:11:11 -0500

    Create Full-Bright.lua

---
+++ Evade/Full-Bright.lua
@@ -0,0 +1,8 @@
+ local light = Instance.new("PointLight", Character.HumanoidRootPart)
+    light.Brightness = .3
+    light.Range = 10000
+    Lighting.TimeOfDay = "14:00:00"
+    Lighting.FogEnd = 10000;
+    Lighting.Brightness = 2;
+    Lighting.Ambient = Color3.fromRGB(255,255,255)
+    Lighting.FogColor = Color3.fromRGB(255,255,255)
