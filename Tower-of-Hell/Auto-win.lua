bce034a41cb285743b3afe76a31228efa3da57d0
YellowGreg <101320329+YellowGreg@users.noreply.github.com>
2023-01-18 13:41:06 -0500

    Create Auto-win.lua

---
+++ Tower-of-Hell/Auto-win.lua
@@ -0,0 +1,10 @@
+local P = game:GetService'Players'
+local HRP = P.LocalPlayer.Character.HumanoidRootPart
+local Sections = game:GetService("Workspace").tower.sections
+for _, Child in next, Sections:GetChildren() do
+  if Child.Name ~= "start" then
+    HRP.CFrame = Child.start.CFrame + Vector3.new(1,2,1)
+    task.wait()
+  end
+end
+HRP.CFrame = Sections.finish.FinishGlow.CFrame
