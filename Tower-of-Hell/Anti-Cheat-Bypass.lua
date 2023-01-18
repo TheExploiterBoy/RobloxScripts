51ff653f1b316a55145998622b4671a1465e2d3b
YellowGreg <101320329+YellowGreg@users.noreply.github.com>
2023-01-18 13:52:01 -0500

    Update Anti-Cheat-Bypass.lua

--- Tower-of-Hell/Anti-Cheat-Bypass.lua
+++ Tower-of-Hell/Anti-Cheat-Bypass.lua
@@ -1,13 +1,32 @@
---// Anti-Cheat Bypass (Can barely call it an anticheat)
-
-for _,v in pairs(getgc()) do
-    if type(v) == "function" and getfenv(v).script == Player.PlayerScripts.LocalScript then
-        if debug.getinfo(v).name == "kick" then
-            hookfunction(v,function()end)
+    local reg = getreg()
+ 
+    for i, Function in next, reg do
+        if type(Function) == 'function' then
+            local info = getinfo(Function)
+ 
+            if info.name == 'kick' then
+                if (hookfunction(info.func, function(...)end)) then
+                    print'succesfully hooked kick'
+                else
+                    print'failed to hook kick'
+                end
+            end
         end
     end
-end
-
-if Player.PlayerScripts:FindFirstChild("LocalPartScript") then
-    Player.PlayerScripts.LocalPartScript:Destroy()
-end
+    local playerscripts = game:GetService'Players'.LocalPlayer.PlayerScripts
+ 
+    local script1 = playerscripts.LocalScript
+    local script2 = playerscripts.LocalScript2
+ 
+    local script1signal = script1.Changed
+    local script2signal = script2.Changed
+ 
+    for i, connection in next, getconnections(script1signal) do
+        connection:Disable()
+    end
+    for i, connection in next, getconnections(script2signal) do
+        connection:Disable()
+    end
+ 
+    script1:Destroy()
+    script2:Destroy()
