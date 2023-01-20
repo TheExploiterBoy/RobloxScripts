6e9d7e7ef84caeea792207eeca4cb24c32f7b595
YellowGreg <101320329+YellowGreg@users.noreply.github.com>
2023-01-20 17:59:58 -0500

    Create Kill-In-Platform.lua

---
+++ Zombie-Attack/Kill-In-Platform.lua
@@ -0,0 +1,42 @@
+plr = game:service'Players'.LocalPlayer
+char = plr.Character
+root = char.HumanoidRootPart
+iszombie = false
+ 
+platform = Instance.new('Part', workspace)
+platform.Size = Vector3.new(100,0,100)
+platform.Anchored = true
+platform.Position = Vector3.new(555,555,555)
+root.CFrame = platform.CFrame * CFrame.new(0,4,0)
+ 
+if workspace:FindFirstChild(plr.Name) then
+   iszombie = false
+   warn'You are not a zombie!'
+else
+   iszombie = true
+   warn'You are a zombie!'
+end
+ 
+wait(.5)
+ 
+if not iszombie then
+   for _, a in pairs(workspace.enemies:children()) do
+       for _, b in pairs(a:children()) do
+           if b:IsA'Part' then
+               b.Anchored = true
+               b.CFrame = root.CFrame * CFrame.new(2,0,2)
+           end
+       end
+   end
+elseif iszombie then
+   for _, a in pairs(game:service'Players':GetPlayers()) do
+       if a.Character then
+           for _, b in pairs(a.Character:children()) do
+               if b:IsA'Part' and a.Name ~= plr.Name then
+                   b.Anchored = true
+                   b.CFrame =  root.CFrame * CFrame.new(2,0,2)
+               end
+           end
+       end
+   end
+end
