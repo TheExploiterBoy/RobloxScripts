128fc54429fe7c96138b011f68cd3290ce155789
YellowGreg <101320329+YellowGreg@users.noreply.github.com>
2023-01-20 17:45:38 -0500

    Create Equip-All-Knife.lua

---
+++ Zombie-Attack/Equip-All-Knife.lua
@@ -0,0 +1,7 @@
+--// i could have made this script but someone else made it :( \\--
+
+for _,Thing in pairs(game.ReplicatedStorage.Knives:GetChildren()) do
+if Thing:IsA("Tool") then
+Thing.Parent = game.Players.LocalPlayer.Backpack
+end
+end
